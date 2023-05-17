3db4161a12c6c1da4ff65fdbc0ca575234f37f6c
rachintemi <67642324+rachintemi@users.noreply.github.com>
2023-05-17 10:34:13 +0200

    1 file modified

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import useFetch from '../hook/useHook';
 
 const HomePage = () => {
-  const url =
-    'https://services.odata.org/TripPinRESTierService/(S(hespbvdrrmhquk5vqlzcpbro))/People';
-  const { data, loading, error } = useFetch(url);
-  console.log(data);
-  return <div> {data}</div>;
+  const { data, loading, error } = useFetch(
+    'https://services.odata.org/TripPinRESTierService/(S(hespbvdrrmhquk5vqlzcpbro))/People'
+  );
+
+  return <div> </div>;
 };
 
 export default HomePage;
